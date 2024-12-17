86ce9f07b77483d58189dcb9a07a4243eef2421b
Adelina <adeline.selivanovskaia@toptal.com>
2024-12-17 18:16:38 +0100

    Fix for deploy

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,13 @@
-import { useState } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { TagGame } from "./modules/tag-game/TagGame";
 import { HomePage } from "./modules/home-page/HomePage";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/games/tag" element={<TagGame />}/>
+        <Route path="/games/tag" element={<TagGame />} />
       </Routes>
     </BrowserRouter>
   );
